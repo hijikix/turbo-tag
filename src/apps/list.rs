61eeccc904e0961c2292303f8c1f743ebca296dd
Daisuke Hashimoto <galapagosit@gmail.com>
2025-05-06 23:29:50 +0900

    move_page

--- src/apps/list.rs
+++ src/apps/list.rs
@@ -1,12 +1,30 @@
+use std::collections::HashMap;
+
+use crate::anchor::{move_to_page, Anchor, AppPage, NextPage};
+
 #[derive(serde::Deserialize, serde::Serialize, Default)]
 pub struct ListApp {
     counter: u32,
+}
+
+impl AppPage for ListApp {
+    fn on_move_page(&mut self, _params: &HashMap<String, String>) {}
 }
 
 impl eframe::App for ListApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("list page");
+            if ui.button("move to detail").clicked() {
+                let next_page = NextPage {
+                    anchor: Anchor::Detail,
+                    params: HashMap::from([
+                        (String::from("key1"), String::from("value1")),
+                        (String::from("key2"), String::from("value2")),
+                    ]),
+                };
+                move_to_page(ctx, next_page)
+            }
         });
     }
 }
