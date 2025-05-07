5d91bded08ef0d6d4481c59f0b7d85b759c7cabd
Daisuke Hashimoto <galapagosit@gmail.com>
2025-05-08 03:24:39 +0900

    fix AppPage

--- src/apps/detail.rs
+++ src/apps/detail.rs
@@ -12,13 +12,6 @@ impl AppPage for DetailApp {
     }
 }
 
-impl AppPage for DetailApp {
-    fn on_move_page(&mut self, params: &HashMap<String, String>) {
-        println!("DetailApp on_move_page");
-        println!("{:?}", params);
-    }
-}
-
 impl eframe::App for DetailApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
